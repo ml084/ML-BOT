77ce2978b1baa969eaa8387a851200724bd9b763
ml084 <minecraftlearners084@gmail.com>
2024-08-10 15:56:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["ML COMMUNITY","CREATED BY ML, ML ON TOP, WATCHING MEMBERS"];
 
 
 let currentIndex = 0;
